2c70e1c5f655e887614c87f82129016dfec95b55
momos69 <136116918+momos69@users.noreply.github.com>
2023-07-13 01:47:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+/*==Testimonial Section==*/
+
+var swiper = new Swiper(".testimonial-wrapper", {
+  loop:'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+});
+
+
+
+
+ 
